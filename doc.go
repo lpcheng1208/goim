ddedba54ce4309e7fc8856abb126a97c7ea08597
tsingsonqin <tsingson@gmail.com>
2019-04-21 12:40:19 +0800

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,18 @@
+package goim
+
+/**
+goim is a im server writen by golang.
+
+## Features
+ * Light weight
+ * High performance
+ * Pure Golang
+ * Supports single push, multiple push and broadcasting
+ * Supports one key to multiple subscribers (Configurable maximum subscribers count)
+ * Supports heartbeats (Application heartbeats, TCP, KeepAlive, HTTP long pulling)
+ * Supports authentication (Unauthenticated user can't subscribe)
+ * Supports multiple protocols (WebSocket，TCP，HTTP）
+ * Scalable architecture (Unlimited dynamic job and logic modules)
+ * Asynchronous push notification based on Kafka
+
+ */
